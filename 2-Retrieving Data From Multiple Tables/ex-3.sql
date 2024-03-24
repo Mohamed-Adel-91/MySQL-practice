73c949a9147800a68272f5d593a0d818a8565c92
Mohamed Adel <mohamed101291@gmail.com>
2024-03-24 15:10:30 +0200

    2-Retrieving Data From Multiple Tables

---
+++ 2-Retrieving Data From Multiple Tables/ex-3.sql
@@ -0,0 +1,11 @@
+SELECT 
+	e.employee_id,
+    e.first_name,
+    m.first_name AS manager
+FROM
+	sql_hr.employees e
+JOIN 
+	sql_hr.employees m
+    ON e.reports_to = m.employee_id
+    
+-- this is called self join / we join the same table with deferant alias
